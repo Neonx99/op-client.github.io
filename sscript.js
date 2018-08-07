b0122e14166d6eff3772a12490244393c31e3575
opclient <30980283+opclient@users.noreply.github.com>
2018-08-07 12:49:18 +0700

    asd

--- sscript.js
+++ sscript.js
@@ -117,10 +117,10 @@ if (window.location.origin == "http://agar.io") {
                         offset += 2;
                         let coins = buf.getFloat64(offset, true);
                         offset += 2;
-                        if (spawnedBots >= maxBots) {
+                        if (connectedBots >= maxBots) {
                             $("#botCounter").html(maxBots + "/" + maxBots);
                         } else {
-                            $("#botCounter").html(spawnedBots + "/" + maxBots);
+                            $("#botCounter").html(connectedBots + "/" + maxBots);
                         }
                         $('#coinsCounter').html(`${coins}`);
                     }
@@ -131,9 +131,9 @@ if (window.location.origin == "http://agar.io") {
                         let numBots = buf.getUint16(offset, true);
                         offset += 2;
                         for (let i = 0; i < numBots; i++) {
-                            let xPos = buf.getInt32(offset, true) + window.offsetX;
+                            let xPos = buf.getInt32(offset, true);
                             offset += 4;
-                            let yPos = buf.getInt32(offset, true) + window.offsetY;
+                            let yPos = buf.getInt32(offset, true);
                             offset += 4;
                             window.bots.push({
                                 "xPos": xPos,
@@ -302,7 +302,7 @@ if (window.location.origin == "http://agar.io") {
             const botMode = localStorage.getItem('botMode');
             $('head').append(`<style type="text/css">.agario-panel,.shop-blocker{border-top: 5px solid #09f4ff; background-image: url("http://cdn.ogario.ovh/static/img/pattern.png"); background-repeat: repeat; background-position: top center;}</style>`);
             $('.partymode-info').remove();
-            $('.agario-promo-container').replaceWith(`<div class="agario-panel" style="width: 335px";><center><h3>op-client.tk</h3></center><input onkeypress="return event.charCode >= 48 && event.charCode <= 57"onchange="localStorage.setItem('botAmount', this.value);window.client.botAmount=this.value;"id="botAmount"maxlength="4"class="form-control"placeholder="Bot Amount"value="${botAmount}"></input><br></span><button id="toggleButton" onclick="window.client.startBots();" class="btn btn-success">Start Bots</button><button onclick="if(!window.client.reconnect&&window.client.ws.readyState!==1){window.client.reconnect=true;window.client.connect();}else{}" class="btn btn-success" id="reconnectButton" style="float:right;">Reconnect</button></div>`);
+            $('.agario-promo-container').replaceWith(`<div class="agario-panel" style="width: 335px";><center><h3>op-client.tk</h3></center><input onkeypress="return event.charCode >= 48 && event.charCode <= 57"onchange="localStorage.setItem('botAmount', this.value);window.client.botAmount=this.value;"id="botAmount"maxlength="3"class="form-control"placeholder="Bot Amount"value="${botAmount}"></input><br></span><button id="toggleButton" onclick="window.client.startBots();" class="btn btn-success">Start Bots</button><button onclick="if(!window.client.reconnect&&window.client.ws.readyState!==1){window.client.reconnect=true;window.client.connect();}else{}" class="btn btn-success" id="reconnectButton" style="float:right;">Reconnect</button></div>`);
         }
 
         removeElements() {
@@ -481,13 +481,12 @@ if (window.location.origin == "http://agar.io") {
     }
 
 
-    $.ajax('http://agar.io/agario.core.js', { // edit core for get server IP, player position and etc
+    $.ajax('http://agar.io/agario.core.js', {
         success: core => {
             core = core.replace(/([\w$]+\(\d+,\w\[\w>>2\]\|0,(\+\w),(\+\w)\)\|0;[\w$]+\(\d+,\w\[\w>>2\]\|0,\+-(\+\w\[\w\+\d+>>3\]),\+-(\+\w\[\w\+\d+>>3\])\)\|0;)/i, '$1 window.viewScale=$2; if(window.core.setFpsCap) {window.core.setFpsCap(999)};if (window.coordOffsetFixed) { window.playerX=$4+window.offsetX; window.playerY=$5+window.offsetY;} if(window.draw){window.draw();}');
             core = core.replace(/(\w\[\w\+(\d+)>>3]=(\w);\w\[\w\+(\d+)>>3]=(\w);\w\[\w\+(\d+)>>3]=(\w);\w\[\w\+(\d+)>>3]=(\w);\w\=\w\+(\d+)\|(\d+);)/i, '$1 function setMapCoords(_0x7e8bx1, _0x7e8bx2, _0x7e8bx3, _0x7e8bx4, _0x7e8bx5, _0x7e8bx6) { if (_0x7e8bx6 - _0x7e8bx5 == 24) { if (_0x7e8bx3 - _0x7e8bx1 > 14E3) { if (_0x7e8bx4 - _0x7e8bx2 > 14E3) { window.offsetX = 7071.067811865476 - _0x7e8bx3; window.offsetY = 7071.067811865476 - _0x7e8bx4; window.minX = _0x7e8bx1;window.minY=_0x7e8bx2;window.maxX=_0x7e8bx3;window.maxY=_0x7e8bx4; window.coordOffsetFixed = true; } } } } setMapCoords($3,$5,$7,$9,$2,$8);');
             core = core.replace(/var (\w)=new WebSocket\((\w\(\w\))\);/, 'window.client.agarServer=$2;var $1=new WebSocket(window.client.agarServer);');
             core = core.replace(/(function\(\w\){)(\w.\w\[\w\].stroke\(\))(})/, '$1 $3');
-            core = core.replace(/if\((\+\w\[\w>>3\])<1\.0\){/i, 'if($1<!client.extraZoom){return;} else {return;}');
             eval(core);
         },
         dataType: 'text',
